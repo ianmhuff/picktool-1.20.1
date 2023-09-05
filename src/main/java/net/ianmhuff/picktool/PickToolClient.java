4fecf35f315690388c959b8a65478a1e106f7d16
Ian Huff <ianmhuff@gmail.com>
2023-09-05 01:13:23 -0400

    attempt 2

---
+++ src/main/java/net/ianmhuff/picktool/PickToolClient.java
@@ -0,0 +1,10 @@
+package net.ianmhuff.picktool;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PickToolClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
